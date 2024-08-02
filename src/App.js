d7e9dcefead663e193fb3603aee2091838d5a95e
Peter O <peterochieng0626@gmail.com>
2024-08-01 21:38:08 -0400

    added some dependencies to the project as well as added some logic to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import SearchBar from "./components/SearchBar";
 import SearchResults from "./components/SearchResults";
 import axios from "axios";
-
+import qs from 'qs';
 
 function App() {
+
+  const clientID = process.env.REACT_APP_SPOTIFY_API_ID;
+  const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+  const authHeader = Buffer.from(`${clientID}:${clientSecret}`).toString('base64');
+  const tokenEndpoint = 'https://accounts.spotify.com/api/token';
+  
+
+  const getAuthToken = async() => {
+    
+    const data = qs.stringify({grant_type: 'client_credentials'})
+    
+  }
+
+
+
   return (
   <div>
     <SearchBar/>
