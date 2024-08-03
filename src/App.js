f6bfd8ca165245751226eb03681f91393c58942b
Peter O <peterochieng0626@gmail.com>
2024-08-03 17:39:40 -0400

    Added logic that enables me to get a token for the Spotify API, I also added some more logic to the SearchBar component that enables Child to Parent communication from the SearchBar component to the App.js component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import SearchBar from "./components/SearchBar";
 import SearchResults from "./components/SearchResults";
 import axios from "axios";
 import qs from 'qs';
+import { Buffer } from "buffer";
+
 
 function App() {
 
@@ -9,19 +11,43 @@ function App() {
   const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
   const authHeader = Buffer.from(`${clientID}:${clientSecret}`).toString('base64');
   const tokenEndpoint = 'https://accounts.spotify.com/api/token';
-  
+  const data = qs.stringify({grant_type: 'client_credentials'})
+
 
   const getAuthToken = async() => {
     
-    const data = qs.stringify({grant_type: 'client_credentials'})
-    
+    const response = await axios.post(tokenEndpoint,data, {
+      headers: {
+        'Authorization': `Basic ${authHeader}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+
+    const accessToken = response.data.access_token;
+
+    console.log(accessToken)
+
+    return accessToken
+
   }
+
+  const handleSubmit = (term) => {
+    console.log('do a search with', term)
+  }
+
+  /*const getSong = async () => {
+
+  }*/
+
+
+
+
 
 
 
   return (
   <div>
-    <SearchBar/>
+    <SearchBar onSubmit = {handleSubmit}/>
     <SearchResults/>
   </div>
   ) 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,23 @@
-function SearchBar () {
+import { useState } from "react";
+
+function SearchBar ({onSubmit}) {
+  const [term, setTerm] = useState('')
+
+  const handleFormSubmit = (e) => {
+    onSubmit(term);
+    e.preventDefault()
+  }
+
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+  }
+
+
+
   return(
     <div>
-      <form>
-        <input/>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange}/>
         <button>Submit</button>
       </form>
     </div>
